0bf3fe4b6a799a5d7b11f591b9c7b6609b859d92
GeElshaddai@github.com <gesitels25@gmail.com>
2024-05-15 18:37:34 +0700

    index changes

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php require 'a.php'; ?>
+<div class="container-fluid px-4">
+  <h1 class="mt-4">Selamat Datang</h1>
+  <ol class="breadcrumb mb-4">
+    <li class="breadcrumb-item active">Dashboard</li>
+  </ol>
+  <div class="row mb-4 mx-3 border-bottom">
+    <div class="col-lg-3 text-center">
+      <img src="library/assets/img/fprofile1.png" alt="" height="200px" width="200px">
+    </div>
+    <div class="col-lg-9 pt-4">
+      <h3>ATMA Hospital</h3>
+      <p style="font-family:Merriweather">Tercatat sebagai pemimpin dalam industri kesehatan, ATMA Hospital menawarkan solusi inovatif dan pelayanan terbaik untuk memenuhi kebutuhan kesehatan masyarakat. Dengan komitmen kami terhadap pencegahan, pengobatan, dan perawatan yang holistik, kami berdedikasi untuk meningkatkan kualitas hidup setiap individu melalui pendekatan yang berfokus pada pasien dan teknologi canggih.</p>
+    </div>
+  </div>
+</div>
 <?php require 'b.php'; ?>
